36994ee0a71c1da5243749cc9c404a382d792a71
Shoujii <retender.jw@gmail.com>
2017-12-10 14:49:52 +0100

    -

--- update_script.sh
+++ update_script.sh
@@ -17,17 +17,38 @@
 #-------------------------------------------------------------------------------------------------------------
 
 update_script() {
+SCRIPT_PATH="/root/NeXt-Server"
 
 git remote update
 if ! git diff --quiet origin/master; then
+  
   mkdir -p /root/backup_next_server
 
   ### add more important stuff to backup ###
-  mkdir -p /root/backup_next_server/logs
-  cp ${SCRIPT_PATH}/logs/* /root/backup_next_server/logs/
-  cp ${SCRIPT_PATH}/login_information /root/backup_next_server/
-  cp ${SCRIPT_PATH}/ssh_privatekey.txt /root/backup_next_server/
-  cp ${SCRIPT_PATH}/installation_times.txt /root/backup_next_server/
+  if [ -d "${SCRIPT_PATH}/logs/"] ; then
+    mkdir -p /root/backup_next_server/logs
+    cp ${SCRIPT_PATH}/logs/* /root/backup_next_server/logs/
+  fi
+
+  if [ -e ${SCRIPT_PATH}/login_information ]; then
+    cp ${SCRIPT_PATH}/login_information /root/backup_next_server/
+  fi
+
+  if [ -e ${SCRIPT_PATH}/ssh_privatekey.txt ]; then
+    cp ${SCRIPT_PATH}/ssh_privatekey.txt /root/backup_next_server/
+  fi
+
+  if [ -e ${SCRIPT_PATH}/installation_times.txt ]; then
+    cp ${SCRIPT_PATH}/installation_times.txt /root/backup_next_server/
+  fi
+
+  if [ -e ${SCRIPT_PATH}/configs/userconfig.cfg ]; then
+    cp ${SCRIPT_PATH}/configs/userconfig.cfg /root/backup_next_server/
+  fi
+
+  if [ -e ${SCRIPT_PATH}/configs/versions.cfg ]; then
+    cp ${SCRIPT_PATH}/configs/versions.cfg /root/backup_next_server/
+  fi
 
   #reset branch
   cd ${SCRIPT_PATH}
@@ -35,8 +56,34 @@ if ! git diff --quiet origin/master; then
   git reset --hard origin/master
 
   #restore backup
-  cp /root/backup_next_server/logs/* ${SCRIPT_PATH}/logs/
-  cp /root/backup_next_server/* ${SCRIPT_PATH}/
+  if [ -d "/root/backup_next_server/logs/"] ; then
+    cp /root/backup_next_server/logs/* ${SCRIPT_PATH}/logs/
+  fi
+
+  if [ -e /root/backup_next_server/login_information ]; then
+    cp /root/backup_next_server/login_information ${SCRIPT_PATH}/
+  fi
+
+  if [ -e /root/backup_next_server/ssh_privatekey.txt ]; then
+    cp /root/backup_next_server/ssh_privatekey.txt ${SCRIPT_PATH}/
+  fi
+
+  if [ -e /root/backup_next_server/installation_times.txt ]; then
+    cp /root/backup_next_server/installation_times.txt ${SCRIPT_PATH}/
+  fi
+
+  if [ -e /root/backup_next_server/userconfig.cfg ]; then
+    cp /root/backup_next_server/userconfig.cfg ${SCRIPT_PATH}/configs/
+  fi
+
+  if [ -e /root/backup_next_server/versions.cfg ]; then
+    cp /root/backup_next_server/versions.cfg ${SCRIPT_PATH}/configs/
+  fi
+
+
+  if [ -e ${SCRIPT_PATH}/configs/versions.cfg ]; then
+    cp ${SCRIPT_PATH}/configs/versions.cfg /root/backup_next_server/
+  fi
 
   GIT_LOCAL_FILES_HEAD=$(git rev-parse --short HEAD)
 else
