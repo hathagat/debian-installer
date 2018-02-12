e025935fcc882009ddab75baec9fe4ef5af1c2b9
shoujii <retender.jw@gmail.com>
2018-02-12 22:20:20 +0100

    -

--- script/mariadb.sh
+++ script/mariadb.sh
@@ -18,18 +18,26 @@
 
 install_mariadb() {
 
+DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common dirmngr >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install software-properties-common dirmngr packages"
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8 >>"${main_log}" 2>>"${err_log}"
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.n-ix.net/mariadb/repo/10.2/debian stretch main' >>"${main_log}" 2>>"${err_log}"
+apt-get update -y >/dev/null 2>&1
+
+MYSQL_ROOT_PASS=$(password)
+
+debconf-set-selections <<< "mariadb-server mysql-server/root_password password ${MYSQL_ROOT_PASS}"
+debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password ${MYSQL_ROOT_PASS}"
+
 DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install mariadb-server package"
 
-MYSQLUSER="NXTDBUSER"
-MYSQLUSERPW=$(password)
-
-mysql -u root -e "CREATE USER '${MYSQLUSER}'@'localhost' IDENTIFIED BY '${MYSQLUSERPW}';GRANT ALL PRIVILEGES ON * . * TO '${MYSQLUSER}'@'localhost';FLUSH PRIVILEGES;"
-mysql -u ${MYSQLUSER} -p${MYSQLUSERPW} -e "DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;" >>"${main_log}" 2>>"${err_log}"
-
 echo "#------------------------------------------------------------------------------#" >> ${SCRIPT_PATH}/login_information
-echo "${MYSQLUSER}: ${MYSQLUSERPW}" >> ${SCRIPT_PATH}/login_information
+echo "MYSQL_ROOT_PASS: $MYSQL_ROOT_PASS" >> ${SCRIPT_PATH}/login_information
 echo "#------------------------------------------------------------------------------#" >> ${SCRIPT_PATH}/login_information
 echo "" >> ${SCRIPT_PATH}/login_information
 
+#mysqladmin -u root password ${MYSQL_ROOT_PASS}
+
 sed -i 's/.*max_allowed_packet.*/max_allowed_packet = 128M/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+#mysql -u root -p${MYSQL_ROOT_PASS} -e "DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;" >>"${main_log}" 2>>"${err_log}"
 }
