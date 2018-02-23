389cdf14da4c8dd38c750440b6325733db5adb65
hathagat <7647228+hathagat@users.noreply.github.com>
2018-02-23 22:02:27 +0100

    Add install step for common tools
    curl, htop, vim, bash-it

--- install.sh
+++ install.sh
@@ -34,52 +34,40 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 	fi
 
 	system_start=`date +%s`
-	echo "1" | dialog --gauge "Installing System..." 10 70 0
+	echo "5" | dialog --gauge "Updating system..." 10 70 0
 	source ${SCRIPT_PATH}/script/system.sh; install_system
-	if [[ ${USE_MAILSERVER} = "0" ]]; then
-		sed -i '/mail/d' /etc/hosts
-		rm /etc/mailname
-	fi
 	system_end=`date +%s`
 	systemtime=$((system_end-system_start))
 
 	common_start=`date +%s`
 	if [[ ${INSTALL_COMMON} = "1" ]]; then
-	    echo "3" | dialog --gauge "Installing Common Tools..." 10 70 0
-		apt-get -y install curl htop vim >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install common packages"
-
-		git clone --depth=1 git://github.com/amix/vimrc.git ~/.vim_runtime >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to clone vimrx"
-		bash ~/.vim_runtime/install_basic_vimrc.sh >>"${main_log}" 2>>"${err_log}"
+	    echo "10" | dialog --gauge "Installing common stuff..." 10 70 0
+	    source ${SCRIPT_PATH}/script/common.sh; install_common
 	fi
 	common_end=`date +%s`
 	commontime=$((common_end-common_start))
 
 	openssl_start=`date +%s`
-	echo "5" | dialog --gauge "Installing OpenSSL..." 10 70 0
+	echo "20" | dialog --gauge "Installing OpenSSL..." 10 70 0
 	source ${SCRIPT_PATH}/script/openssl.sh; install_openssl
 	openssl_end=`date +%s`
 	openssltime=$((openssl_end-openssl_start))
 
 	openssh_start=`date +%s`
-	echo "10" | dialog --gauge "Installing OpenSSH..." 10 70 0
+	echo "30" | dialog --gauge "Installing OpenSSH..." 10 70 0
 	source ${SCRIPT_PATH}/script/openssh.sh; install_openssh
-
-	if [[ ${FIX_SSH_PORT} = "1" ]]; then
-		sed -i 's/Port .*/Port ${FIXED_SSH_PORT}/g' /etc/ssh/sshd_config
-	fi
 	openssh_end=`date +%s`
 	opensshtime=$((openssh_end-openssh_start))
 
 	fail2ban_start=`date +%s`
-	echo "15" | dialog --gauge "Installing fail2ban..." 10 70 0
+	echo "40" | dialog --gauge "Installing fail2ban..." 10 70 0
 	source ${SCRIPT_PATH}/script/fail2ban.sh; install_fail2ban
 	fail2ban_end=`date +%s`
 	fail2bantime=$((fail2ban_end-fail2ban_start))
 
 	mariadb_start=`date +%s`
-	echo "20" | dialog --gauge "Installing MariaDB..." 10 70 0
 	if [[ ${INSTALL_MARIADB} = "1" ]]; then
-	    echo "20" | dialog --gauge "Installing MariaDB..." 10 70 0
+	    echo "50" | dialog --gauge "Installing MariaDB..." 10 70 0
 		source ${SCRIPT_PATH}/script/mariadb.sh; install_mariadb
 	fi
 	maria_end=`date +%s`
@@ -87,17 +75,17 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 
 	nginx_start=`date +%s`
 	if [[ ${INSTALL_NGINX} = "1" ]]; then
-	    echo "25" | dialog --gauge "Installing Nginx Addons..." 10 70 0
+	    echo "60" | dialog --gauge "Installing Nginx Addons..." 10 70 0
 	    source ${SCRIPT_PATH}/script/nginx_addons.sh; install_nginx_addons
 
-	    echo "30" | dialog --gauge "Installing Nginx..." 10 70 0
+	    echo "65" | dialog --gauge "Installing Nginx..." 10 70 0
 	    source ${SCRIPT_PATH}/script/nginx.sh; install_nginx
 
-	    echo "50" | dialog --gauge "Installing LE..." 10 70 0
+	    echo "70" | dialog --gauge "Installing Lets Encrypt..." 10 70 0
 	    source ${SCRIPT_PATH}/script/lets_encrypt.sh; install_lets_encrypt
 	    source ${SCRIPT_PATH}/script/lets_encrypt.sh; create_nginx_cert
 
-	    echo "70" | dialog --gauge "Installing Nginx Vhost..." 10 70 0
+	    echo "75" | dialog --gauge "Installing Nginx vHost..." 10 70 0
 	    source ${SCRIPT_PATH}/script/nginx_vhost.sh; install_nginx_vhost
 	fi
 	nginx_end=`date +%s`
@@ -105,7 +93,7 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 
 	php_start=`date +%s`
 	if [[ ${INSTALL_PHP} = "1" ]]; then
-	    echo "75" | dialog --gauge "Installing PHP..." 10 70 0
+	    echo "80" | dialog --gauge "Installing PHP..." 10 70 0
         if [[ ${USE_PHP7_1} = "1" ]]; then
 		    source ${SCRIPT_PATH}/script/php7_1.sh; install_php_7_1
 	    fi
@@ -117,9 +105,9 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 	php_end=`date +%s`
 	phptime=$((php_end-php_start))
 
-	echo "85" | dialog --gauge "Installing Mailserver..." 10 70 0
 	mailserver_start=`date +%s`
 	if [[ ${USE_MAILSERVER} = "1" ]]; then
+	    echo "90" | dialog --gauge "Installing Mailserver..." 10 70 0
 		source ${SCRIPT_PATH}/script/unbound.sh; install_unbound
 		source ${SCRIPT_PATH}/script/mailserver.sh; install_mailserver
 		source ${SCRIPT_PATH}/script/dovecot.sh; install_dovecot
@@ -138,7 +126,7 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 	firewalltime=$((firewall_end-firewall_start))
 
 	apt-get -y autoremove >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to autoremove packages"
-	apt-get autoclean
+	apt-get autoclean >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to autoclean packages"
 
 	install_end=`date +%s`
 	runtime=$((install_end-install_start))
---
+++ script/common.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#!/bin/bash
+# Compatible with Ubuntu 16.04 Xenial and Debian 9.x Stretch
+#
+	# This program is free software; you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation; either version 2 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License along
+    # with this program; if not, write to the Free Software Foundation, Inc.,
+    # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#-------------------------------------------------------------------------------------------------------------
+
+install_common() {
+
+# Tools
+apt-get -y install curl htop vim >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install common packages"
+
+git clone --depth=1 git://github.com/amix/vimrc.git ~/.vim_runtime >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to clone vimrx"
+bash ~/.vim_runtime/install_basic_vimrc.sh >>"${main_log}" 2>>"${err_log}"
+
+# DNS
+## CCC       - dns.as250.net
+## OpenNIC   - ns3.cz.dns.opennic.glue
+## DNS.WATCH - resolver2.dns.watch
+cat > /etc/resolv.conf <<END
+domain ${MYDOMAIN}
+search ${MYDOMAIN}
+options rotate
+options timeout:1
+nameserver 194.150.168.168
+nameserver 81.2.241.148
+nameserver 84.200.70.40
+END
+
+# Bash
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to clone bash-it"
+~/.bash_it/install.sh --silent >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install bash-it"
+
+. ~/.bashrc
+bash-it enable plugin docker extract gh git >>"${main_log}" 2>>"${err_log}"
+bash-it enable completion defaults docker docker-compose export gh git git_flow_avh makefile ssh >>"${main_log}" 2>>"${err_log}"
+sed -i "s/bobby/pure/g" ~/.bashrc
+. ~/.bashrc
+
+cat > ~/.bash_profile <<END
+if [ -f ~/.bashrc ]; then
+  . ~/.bashrc
+fi
+END
+
+# User Accounts
+if [[ ${DELETE_USERS} = "1" ]]; then
+    awk -F'[/:]' '{if ($3 >= 1000 && $3 != 65534) print $1}' /etc/passwd | while read name ; do deluser --remove-all-files "$name" >/dev/null 2>&1 ; done
+fi
+
+}
