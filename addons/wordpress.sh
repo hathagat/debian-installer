d508576428f48ff625b40f241ea04287f52ef1ae
BoBBer446 <eXestend@gmx.de>
2018-02-26 15:37:35 +0100

    Absolute testing

--- addons/wordpress.sh
+++ addons/wordpress.sh
@@ -1,1 +1,65 @@
-Test
+#!/bin/bash
+# Compatible with Ubuntu 16.04 Xenial and Debian 9.x Stretch
+#
+	# This program is free software; you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation; either version 2 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License along
+    # with this program; if not, write to the Free Software Foundation, Inc.,
+    # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#-------------------------------------------------------------------------------------------------------------
+
+installwordpress() {
+
+WORDPRESS_USER="wordpressuser"
+WORDPRESS_DB_NAME="wordpressdb"
+
+
+WORDPRESS_DB_PASS=$(password)
+
+MYSQL_ROOT_PASS=$(grep -Pom 1 "(?<=^MYSQL_ROOT_PASS: ).*$" /root/NeXt-Server/login_information)
+#echo "${MYSQL_ROOT_PASS}"
+
+
+mysql -u root -p${MYSQL_ROOT_PASS} -e "CREATE DATABASE ${WORDPRESS_DB_NAME};"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "CREATE USER '${WORDPRESS_USER}'@'localhost' IDENTIFIED BY '${WORDPRESS_DB_PASS}';"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME} . * TO '${WORDPRESS_USER}'@'localhost';"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "FLUSH PRIVILEGES;"
+
+cd root/etc/nginx/html/${MYDOMAIN}/
+#download wordpress
+curl -O https://wordpress.org/latest.tar.gz
+
+#unzip wordpress
+tar -zxvf latest.tar.gz
+#change dir to wordpress
+cd wordpress
+#copy file to parent dir
+cp -rf . ..
+#move back to parent dir
+cd ..
+#remove files from wordpress folder
+rm -R wordpress
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$dbname/g" wp-config.phpperl -pi -e "s/username_here/$dbuser/g" wp-config.phpperl -pi -e "s/password_here/$dbpass/g" wp-config.php
+
+echo "--------------------------------------------" >> ${SCRIPT_PATH}/login_information
+echo "Nextcloud" >> ${SCRIPT_PATH}/login_information
+echo "--------------------------------------------" >> ${SCRIPT_PATH}/login_information
+echo "https://${MYDOMAIN}/nextcloud" >> ${SCRIPT_PATH}/login_information
+echo "Database User: nextcloud" >> ${SCRIPT_PATH}/login_information
+echo "Database password = ${NEXTCLOUD_DB_PASS}" >> ${SCRIPT_PATH}/login_information
+echo "Database name = nextcloud" >> ${SCRIPT_PATH}/login_information
+echo "" >> ${SCRIPT_PATH}/login_information
+echo "" >> ${SCRIPT_PATH}/login_information
+
+}
