1ff221800c4da623fcd8d2d5429b79606339b4b1
BoBBer446 <eXestend@gmx.de>
2018-02-27 01:16:12 +0100

    fix

--- addons/wordpress.sh
+++ addons/wordpress.sh
@@ -56,7 +56,7 @@ SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 while read -r SALT; do
 SEARCH="define('$(echo "$SALT" | cut -d "'" -f 2)"
 REPLACE=$(echo "$SALT" | cut -d "'" -f 4)
-echo "... $SEARCH ... $SEARCH ..."
+#echo "... $SEARCH ... $SEARCH ..."
 sed -i "/^$SEARCH/s/put your unique phrase here/$(echo $REPLACE | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/" /etc/nginx/html/${MYDOMAIN}/wp-config.php
 done <<< "$SALTS"
 
