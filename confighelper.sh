b757f4c81a30a7c96cbb26e4b4c7838a442d9df0
BoBBer446 <eXestend@gmx.de>
2018-03-07 09:02:26 +0100

    fix

--- confighelper.sh
+++ confighelper.sh
@@ -124,7 +124,7 @@ fi
 server_ip=$(ip route get 9.9.9.9 | awk '/9.9.9.9/ {print $NF}')
 sed -i "s/server_ip/$server_ip/g" ${SCRIPT_PATH}/dns_settings.txt
 sed -i "s/yourdomain.com/$MYDOMAIN/g" ${SCRIPT_PATH}/dns_settings.txt
-dialog --title "DNS Settings" --tab-correct --msgbox ${SCRIPT_PATH}/dns_settings.txt 50 200
+dialog --title "DNS Settings" --tab-correct --textbox --ok-label "ok" ${SCRIPT_PATH}/dns_settings.txt 50 200
 
 BACKTITLE="NeXt Server Installation"
 TITLE="NeXt Server Installation"
