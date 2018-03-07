c6aaf7a77a1e43e5c1dd7cb65d16fd1ed2775687
BoBBer446 <eXestend@gmx.de>
2018-03-07 18:07:02 +0100

    add  2>&1

--- cronjobs/startmailman.sh
+++ cronjobs/startmailman.sh
@@ -2,6 +2,5 @@
 # Compatible with Ubuntu 16.04 Xenial and Debian 9.x Stretch
 #Please check the license provided with the script!
 #-------------------------------------------------------------------------------------------------------------
-
 cd /etc/mailman
-npm start
+npm start 2>&1
