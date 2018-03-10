e5b3a5d3075bad05c615f6a19b1f2064d2715acf
Retender <retender.jw@gmail.com>
2018-03-10 12:17:47 +0100

    -

--- cronjobs/startmailman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Compatible with Ubuntu 16.04 Xenial and Debian 9.x Stretch
-#Please check the license provided with the script!
-#-------------------------------------------------------------------------------------------------------------
-cd /etc/mailman
-npm start 2>&1
-
-echo "hi, im a reboot echo, thax for nothing." > /root/reboot.txt 2>&1
--- script/mailman.sh
+++ script/mailman.sh
@@ -48,31 +48,6 @@ pm2 kill
 
 npm start
 
-# Add Cronjob
-chmod ugo+x /root/NeXt-Server/cronjobs/startmailman.sh
-echo "@reboot root /root/NeXt-Server/cronjobs/startmailman.sh" >> /etc/crontab
-#(crontab -l && echo "@reboot /root/NeXt-Server/cronjobs/startmailman.sh") | crontab -
-
-
-# Start script after service mysql ist starts!
-#cat >> /etc/systemd/system/mailman_systemd.service  << 'EOF1'
-#[Unit] After=mysql.service
-
-#[Service]
-#ExecStart=/root/NeXt-Server/cronjobs/startmailman.sh
-
-#[Install]
-#WantedBy=default.target
-#EOF1
-
-#chmod 744 /root/NeXt-Server/cronjobs/startmailman.sh
-#chmod ogo+x /root/NeXt-Server/cronjobs/startmailman.sh
-
-#systemctl daemon-reload
-#systemctl enable mailman_systemd.service
-
-# -------------------------------------------
-
 cat >> /etc/nginx/sites-custom/mailman.conf << 'EOF1'
 location /mailman {
   proxy_pass       http://localhost:4000;
@@ -80,6 +55,8 @@ location /mailman {
   proxy_set_header X-Real-IP $remote_addr;
 }
 EOF1
+
+pm2 startup
 
 echo "#------------------------------------------------------------------------------#" >> ${SCRIPT_PATH}/login_information.txt
 echo "Mailman Address: ${MYDOMAIN}/mailman" >> ${SCRIPT_PATH}/login_information.txt
