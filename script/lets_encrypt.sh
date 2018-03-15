b4be5046c56b603103a9039cb67aeb09254314b6
BoBBer446 <eXestend@gmx.de>
2018-03-15 16:42:04 +0100

    Update to Wildcard
    Try Wildcard

--- script/lets_encrypt.sh
+++ script/lets_encrypt.sh
@@ -18,7 +18,6 @@ sleep 1
 . ~/.bashrc >>"${main_log}" 2>>"${err_log}"
 . ~/.profile >>"${main_log}" 2>>"${err_log}"
 systemctl -q start nginx.service
-
 }
 
 create_nginx_cert() {
@@ -26,7 +25,9 @@ create_nginx_cert() {
 systemctl -q stop nginx.service
 
 cd ${SCRIPT_PATH}/sources/acme.sh/
-bash acme.sh --issue --standalone -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to get let's encrypt cert"
+#bash acme.sh --issue --standalone -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to get let's encrypt cert"
+bash acme.sh --issue --standalone -d *.${MYDOMAIN} --log --dns  dns_cf --keylength ec-384 >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to get let's encrypt cert"
+
 
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/fullchain.cer /etc/nginx/ssl/${MYDOMAIN}-ecc.cer >>"${main_log}" 2>>"${err_log}"
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/${MYDOMAIN}.key /etc/nginx/ssl/${MYDOMAIN}-ecc.key >>"${main_log}" 2>>"${err_log}"
