84386ff6e8fc38ac4a3f224611e72eb05f0a7a70
BoBBer446 <eXestend@gmx.de>
2018-03-21 02:52:04 +0100

    rollback to repo website

--- script/openssl.sh
+++ script/openssl.sh
@@ -10,22 +10,22 @@ mkdir -p ${SCRIPT_PATH}/sources/
 apt-get install -y libtool zlib1g-dev libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install openssl packages"
 
 cd ${SCRIPT_PATH}/sources
-#wget --no-check-certificate https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz --tries=3 >>"${main_log}" 2>>"${err_log}"
-wget https://github.com/openssl/openssl/archive/OpenSSL_${OPENSSL_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
+wget --no-check-certificate https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz --tries=3 >>"${main_log}" 2>>"${err_log}"
 	ERROR=$?
 	if [[ "$ERROR" != '0' ]]; then
-      echo "Error: OpenSSL_${OPENSSL_VERSION}.tar.gz download failed."
+      echo "Error: openssl-${OPENSSL_VERSION}.tar.gz download failed."
       exit
     fi
 
-tar -xzf OpenSSL_${OPENSSL_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
+tar -xzf openssl-${OPENSSL_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
 	ERROR=$?
 	if [[ "$ERROR" != '0' ]]; then
-      echo "Error: OpenSSL_${OPENSSL_VERSION}.tar.gz is corrupted."
+      echo "Error: openssl-${OPENSSL_VERSION}.tar.gz is corrupted."
       exit
     fi
-rm OpenSSL_${OPENSSL_VERSION}.tar.gz
-cd openssl-OpenSSL_${OPENSSL_VERSION}
+rm openssl-${OPENSSL_VERSION}.tar.gz
+
+cd openssl-${OPENSSL_VERSION}
 ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib shared zlib-dynamic -Wl,-R,'$(LIBRPATH)' -Wl,--enable-new-dtags >>"${make_log}" 2>>"${make_err_log}" || error_exit "Failed to configure openssl"
 
 make -j $(nproc) >>"${make_log}" 2>>"${make_err_log}" || error_exit "Failed to make openssl"
