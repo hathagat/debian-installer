f23c220997790d8e17caefa06e28d9a9fc773315
Retender <retender.jw@gmail.com>
2018-03-31 23:41:08 +0200

    -

--- script/php5_6.sh
+++ script/php5_6.sh
@@ -22,11 +22,11 @@ PHPVERSION7="5.6"
 
 if [[ ${DISTOS} == 'UBUNTU' ]]; then
 	install_packages "memcached"
-	DEBIAN_FRONTEND=noninteractive apt-get -y install php$PHPVERSION7-common php$PHPVERSION7-gd php$PHPVERSION7-bcmath php$PHPVERSION7-zip php$PHPVERSION7 php$PHPVERSION7-cli php$PHPVERSION7-common php$PHPVERSION7-curl php$PHPVERSION7-dev php$PHPVERSION7-fpm php$PHPVERSION7-intl php$PHPVERSION7-mcrypt php$PHPVERSION7-mysql php$PHPVERSION7-soap php$PHPVERSION7-sqlite3 php$PHPVERSION7-xsl php$PHPVERSION7-xmlrpc php$PHPVERSION7-json php$PHPVERSION7-opcache php$PHPVERSION7-readline php$PHPVERSION7-xml php$PHPVERSION7-mbstring >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install php 5"
+	install_packages "install php$PHPVERSION7-common php$PHPVERSION7-gd php$PHPVERSION7-bcmath php$PHPVERSION7-zip php$PHPVERSION7 php$PHPVERSION7-cli php$PHPVERSION7-common php$PHPVERSION7-curl php$PHPVERSION7-dev php$PHPVERSION7-fpm php$PHPVERSION7-intl php$PHPVERSION7-mcrypt php$PHPVERSION7-mysql php$PHPVERSION7-soap php$PHPVERSION7-sqlite3 php$PHPVERSION7-xsl php$PHPVERSION7-xmlrpc php$PHPVERSION7-json php$PHPVERSION7-opcache php$PHPVERSION7-readline php$PHPVERSION7-xml php$PHPVERSION7-mbstring"
 fi
 
 if [[ ${DISTOS} == 'DEBIAN' ]]; then
-	DEBIAN_FRONTEND=noninteractive apt-get -y install php$PHPVERSION7-gd php$PHPVERSION7-bcmath php$PHPVERSION7-zip php$PHPVERSION7 php$PHPVERSION7-cli php$PHPVERSION7-common php$PHPVERSION7-curl php$PHPVERSION7-dev php$PHPVERSION7-fpm php$PHPVERSION7-intl php$PHPVERSION7-mcrypt php$PHPVERSION7-mysql php$PHPVERSION7-soap php$PHPVERSION7-sqlite3 php$PHPVERSION7-xsl php$PHPVERSION7-xmlrpc php$PHPVERSION7-json php$PHPVERSION7-opcache php$PHPVERSION7-readline php$PHPVERSION7-xml php$PHPVERSION7-mbstring php$PHPVERSION7-memcached >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install php 5"
+	install_packages "php$PHPVERSION7-gd php$PHPVERSION7-bcmath php$PHPVERSION7-zip php$PHPVERSION7 php$PHPVERSION7-cli php$PHPVERSION7-common php$PHPVERSION7-curl php$PHPVERSION7-dev php$PHPVERSION7-fpm php$PHPVERSION7-intl php$PHPVERSION7-mcrypt php$PHPVERSION7-mysql php$PHPVERSION7-soap php$PHPVERSION7-sqlite3 php$PHPVERSION7-xsl php$PHPVERSION7-xmlrpc php$PHPVERSION7-json php$PHPVERSION7-opcache php$PHPVERSION7-readline php$PHPVERSION7-xml php$PHPVERSION7-mbstring php$PHPVERSION7-memcached"
 fi
 
 #cp ${SCRIPT_PATH}/configs/php/php.ini /etc/php/$PHPVERSION7/fpm/php.ini
