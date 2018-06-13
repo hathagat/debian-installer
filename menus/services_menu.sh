3465d4ae1912635a999db4890df5f4c021ac0fa0
Retender <retender.jw@gmail.com>
2018-06-13 21:28:45 +0200

    .

--- menus/services_menu.sh
+++ menus/services_menu.sh
@@ -14,19 +14,18 @@ source ${SCRIPT_PATH}/script/prerequisites.sh; prerequisites
 
 HEIGHT=40
 WIDTH=80
-CHOICE_HEIGHT=8
+CHOICE_HEIGHT=7
 BACKTITLE="NeXt Server"
 TITLE="NeXt Server"
 MENU="Choose one of the following options:"
 
 		OPTIONS=(1 "Openssh Options"
              2 "Fail2ban Options"
-             3 "Nginx vHost Options"
-             4 "PHP 7.x Options"
-             5 "Lets Encrypt Options"
-             6 "Firewall Options"
-             7 "Back"
-						 8 "Exit")
+             3 "PHP 7.x Options"
+             4 "Lets Encrypt Options"
+             5 "Firewall Options"
+             6 "Back"
+						 7 "Exit")
 
 						 CHOICE=$(dialog --clear \
 										 --nocancel \
@@ -48,21 +47,18 @@ MENU="Choose one of the following options:"
 	source ${SCRIPT_PATH}/menus/fail2ban_menu.sh; menu_options_fail2ban
 	;;
 3)
-	source ${SCRIPT_PATH}/menus/vhost_menu.sh; menu_options_nginx_vhost
-	;;
-4)
 	source ${SCRIPT_PATH}/menus/php_7_x_menu.sh; php_7_x_config
 	;;
-5)
+4)
 	source ${SCRIPT_PATH}/menus/lets_encrypt_menu.sh; menu_options_lets_encrypt
 	;;
-6)
+5)
 	source ${SCRIPT_PATH}/menus/firewall_menu.sh; menu_options_firewall
 	;;
-7)
+6)
   bash ${SCRIPT_PATH}/nxt.sh;
   ;;
-8)
+7)
 	echo "Exit"
 	exit 1
 	;;
