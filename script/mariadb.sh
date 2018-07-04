2046f4bb4ba7a7afaa0ad242006896631803ca0b
Retender <retender.jw@gmail.com>
2018-07-05 00:21:16 +0200

    mariadb

--- script/mariadb.sh
+++ script/mariadb.sh
@@ -7,7 +7,7 @@ install_mariadb() {
 
 install_packages "software-properties-common dirmngr"
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8 >>"${main_log}" 2>>"${err_log}"
-add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.n-ix.net/mariadb/repo/10.2/debian stretch main' >>"${main_log}" 2>>"${err_log}"
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirror.netcologne.de/mariadb/repo/10.3/debian stretch main' >>"${main_log}" 2>>"${err_log}"
 apt-get update -y >/dev/null 2>&1
 
 MYSQL_ROOT_PASS=$(password)
