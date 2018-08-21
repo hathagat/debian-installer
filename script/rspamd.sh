0d39f8806fb53d2d3ce5fba666a05e7a080e0e0e
Retender <retender.jw@gmail.com>
2018-08-21 16:54:33 +0200

    Update rspamd.sh

--- script/rspamd.sh
+++ script/rspamd.sh
@@ -38,17 +38,28 @@ echo "" >> ${SCRIPT_PATH}/login_information.txt
 
 RSPAMADM_PASSWORT_HASH=$(rspamadm pw -p ${RSPAMADM_PASSWORT})
 
+grep 'sse3\|pni' /proc/cpuinfo > /dev/null
+if [ $? -eq 0 ];  then
 cat > /etc/rspamd/local.d/worker-controller.inc <<END
 password = "${RSPAMADM_PASSWORT_HASH}";
 END
+
+else
+
+cat > /etc/rspamd/local.d/worker-controller.inc <<END
+password = "${RSPAMADM_PASSWORT_HASH}";
+END
+
 sed -i '1d' /etc/rspamd/local.d/worker-controller.inc
 hash_temp=$(</etc/rspamd/local.d/worker-controller.inc)
 
 new_file='password = "'
 rm /etc/rspamd/local.d/worker-controller.inc
+
 cat > /etc/rspamd/local.d/worker-controller.inc <<END
 $new_file$hash_temp
 END
+fi
 
 cp ${SCRIPT_PATH}/configs/rspamd/worker-proxy.inc /etc/rspamd/local.d/worker-proxy.inc
 cp ${SCRIPT_PATH}/configs/rspamd/logging.inc /etc/rspamd/local.d/logging.inc
