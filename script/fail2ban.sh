30e20c9d20dfe0eb4c48efb33ea8e569a4c375c6
BoBBer446 <eXestend@gmx.de>
2018-10-28 10:49:03 +0100

    Update script/fail2ban.sh
    fix something but not ready
    Failed during configuration: Have not found any log file for webserver-w00tw00t jail

--- script/fail2ban.sh
+++ script/fail2ban.sh
@@ -5,15 +5,15 @@
 
 install_fail2ban() {
 
-install_packages "python"
+install_packages "python" >>"${main_log}" 2>>"${err_log}"
 
 mkdir -p ${SCRIPT_PATH}/sources/${FAIL2BAN_VERSION}/ >>"${main_log}" 2>>"${err_log}"
 cd ${SCRIPT_PATH}/sources/${FAIL2BAN_VERSION}/ >>"${main_log}" 2>>"${err_log}"
 
 #wget_tar "https://codeload.github.com/fail2ban/fail2ban/tar.gz/${FAIL2BAN_VERSION}"
 
-wget_tar --tries 42 https://github.com/fail2ban/fail2ban/archive/${FAIL2BAN_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
-tar_file "${FAIL2BAN_VERSION}" >>"${main_log}" 2>>"${err_log}"
+wget --tries 42 https://github.com/fail2ban/fail2ban/archive/${FAIL2BAN_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
+tar -xzvf ${FAIL2BAN_VERSION}.tar.gz >>"${main_log}" 2>>"${err_log}"
 cd fail2ban-${FAIL2BAN_VERSION} >>"${main_log}" 2>>"${err_log}"
 
 python setup.py -q install >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install fail2ban package"
@@ -26,5 +26,5 @@ cp files/debian-initd /etc/init.d/fail2ban >>"${main_log}" 2>>"${err_log}"
 update-rc.d fail2ban defaults >>"${main_log}" 2>>"${err_log}"
 service fail2ban start >>"${main_log}" 2>>"${err_log}"
 
-rm -R ${SCRIPT_PATH}/sources/${FAIL2BAN_VERSION}
+#rm -R ${SCRIPT_PATH}/sources/${FAIL2BAN_VERSION}
 }
