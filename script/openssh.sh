c1dd9de19d5bc3a90e7afc438b842db5ac060b4a
BoBBer446 <eXestend@gmx.de>
2018-10-29 10:20:26 +0100

    Update script/openssh.sh

--- script/openssh.sh
+++ script/openssh.sh
@@ -11,8 +11,9 @@ install_packages "libpam-dev"
 
 cd ${SCRIPT_PATH}/sources
 
-wget_tar "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${OPENSSH_VERSION}.tar.gz"
+wget_tar "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${OPENSSH_VERSION}.tar.gz"
 tar_file "openssh-${OPENSSH_VERSION}.tar.gz"
+
 
 cd openssh-${OPENSSH_VERSION}
  ./configure --prefix=/usr --with-pam --with-zlib --with-ssl-engine --with-ssl-dir=/etc/ssl --sysconfdir=/etc/ssh
