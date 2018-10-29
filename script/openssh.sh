84e339a361cffce5787adea950dba8af4e52e4e6
BoBBer446 <eXestend@gmx.de>
2018-10-29 09:48:26 +0100

    update

--- script/openssh.sh
+++ script/openssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# # Compatible with Debian 9.x Stretch
+# Compatible with Debian 9.x Stretch
 #Please check the license provided with the script!
 #-------------------------------------------------------------------------------------------------------------
 
@@ -17,7 +17,6 @@ cd openssh-${OPENSSH_VERSION}
 make -j $(nproc) >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to make openssh"
 mv /etc/ssh{,.bak}
 make install >>"${main_log}" 2>>"${err_log}"
-
 
 cp ${SCRIPT_PATH}/configs/sshd_config /etc/ssh/sshd_config
 cp ${SCRIPT_PATH}/includes/issue /etc/issue
@@ -56,11 +55,6 @@ echo "#-------------------------------------------------------------------------
 echo "" >> ${SCRIPT_PATH}/login_information.txt
 
 SSH_PASS=$(password)
-
-if [ -z "${SSH_PASS}" ]; then
-    echo "SSH_PASS is unset or set to the empty string, creating new one!"
-    SSH_PASS=$(password)
-fi
 
 echo "#------------------------------------------------------------------------------#" >> ${SCRIPT_PATH}/login_information.txt
 echo "#SSH_PASS: ${SSH_PASS}" >> ${SCRIPT_PATH}/login_information.txt
