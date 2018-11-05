4758de46be912de1d69b2a58d87e28a13f9f2294
Retender <retender.jw@gmail.com>
2018-11-05 10:46:18 +0100

    Update openssh.sh

--- script/openssh.sh
+++ script/openssh.sh
@@ -7,19 +7,7 @@ install_openssh() {
 
 mkdir -p /etc/ssh
 
-install_packages "libpam-dev"
-
-cd ${SCRIPT_PATH}/sources
-
-wget_tar "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${OPENSSH_VERSION}.tar.gz"
-tar_file "openssh-${OPENSSH_VERSION}.tar.gz"
-
-
-cd openssh-${OPENSSH_VERSION}
- ./configure --prefix=/usr --with-pam --with-zlib --with-ssl-engine --with-ssl-dir=/etc/ssl --sysconfdir=/etc/ssh
-make -j $(nproc) >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to make openssh"
-mv /etc/ssh{,.bak}
-make install >>"${main_log}" 2>>"${err_log}"
+install_packages "openssh-server openssh-client libpam-dev"
 
 cp ${SCRIPT_PATH}/configs/sshd_config /etc/ssh/sshd_config
 cp ${SCRIPT_PATH}/includes/issue /etc/issue
