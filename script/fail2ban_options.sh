9b1314577e9f90859e4ddb8880ce324bdb332dc8
shoujii <retender.jw@gmail.com>
2018-12-05 14:53:18 +0100

    -

--- script/fail2ban_options.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# # Compatible with Debian 9.x Stretch
-#Please check the license provided with the script!
-#-------------------------------------------------------------------------------------------------------------
-
-activate_fail2ban_jails() {
-	#placeholder! functions will be added later
-	apt-get update >/dev/null 2>&1
-}
