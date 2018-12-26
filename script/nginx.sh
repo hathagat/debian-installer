e619ba43302424628c754a536d19d0f15c9a6e95
shoujii <retender.jw@gmail.com>
2018-12-26 22:23:37 +0100

    -

--- script/nginx.sh
+++ script/nginx.sh
@@ -93,10 +93,7 @@ sed -i "s/MYDOMAIN/${MYDOMAIN}/g" /etc/nginx/sites-available/${MYDOMAIN}.conf
 sed -i "s/IPADR/${IPADR}/g" /etc/nginx/sites-available/${MYDOMAIN}.conf
 
 if [[ ${USE_PHP7_2} == '1' ]]; then
-	sed -i 's/fastcgi_pass unix:\/var\/run\/php\/php7.1-fpm.sock\;/fastcgi_pass unix:\/var\/run\/php\/php7.2-fpm.sock\;/g' /etc/nginx/sites-available/${MYDOMAIN}.conf >>"${main_log}" 2>>"${err_log}"
-	sed -i 's/fastcgi_pass unix:\/var\/run\/php\/php7.1-fpm.sock\;/fastcgi_pass unix:\/var\/run\/php\/php7.2-fpm.sock\;/g' /etc/nginx/_php_fastcgi.conf >>"${main_log}" 2>>"${err_log}"
-	
-	#Fallback
+	sed -i "s/php7.1/php7.2/g" /etc/nginx/sites-available/${MYDOMAIN}.conf  >>"${main_log}" 2>>"${err_log}"
 	sed -i "s/php7.1/php7.2/g" /etc/nginx/_php_fastcgi.conf >>"${main_log}" 2>>"${err_log}"
 fi
 
